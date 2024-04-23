f751697fd3de6dbc2a4cb9bc86273018b2255d84
theonekingboo <139255943+theonekingboo@users.noreply.github.com>
2024-04-22 20:35:37 -0400

    added transparency

--- Neovim/chadrc.lua
+++ Neovim/chadrc.lua
@@ -1,6 +1,9 @@
 ---@type ChadrcConfig 
 local M = {}
-M.ui = {theme = 'chadracula'}
+M.ui = {
+  theme = 'chadracula',
+  transparency = true,
+  }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
